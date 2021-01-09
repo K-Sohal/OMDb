eba512d3428446ba3ba39f9c8a349862384d5cba
KSohal <kavannn@live.com>
2021-01-09 15:17:44 +0000

    Overloaded constructors in service class so that now only the OMDbService needs to be instantiated when creating a DTO.

--- OMDb/OMDbService/HTTPManager/OMDbCallManager.cs
+++ OMDb/OMDbService/HTTPManager/OMDbCallManager.cs
@@ -8,23 +8,24 @@ namespace OMDb
 	{
 		private readonly IRestClient _client;
 
-		public string StringContent { get; set; }
-
 		public OMDbCallManager()
 		{
 			_client = new RestClient(OMDbConfigReader.BaseUrl);
 		}
 
-		public void GetMovieByTitle(string movieTitle, string year = "", string type = "")
+		public string GetMovieByTitle(string movieTitle, string year, string season, string episode, string type)
 		{
-			var request = new RestRequest(OMDbConfigReader.ApiUrlMod + OMDbConfigReader.ApiKey + "&t=" + movieTitle + "&y=" + year + "&type=" + type);
-			StringContent = _client.Execute(request, Method.GET).Content;
+			var request = new RestRequest(OMDbConfigReader.ApiUrlMod + OMDbConfigReader.ApiKey + "&t=" + movieTitle + "&y=" + year + "&Season=" + season + "&Episode=" + episode + "&type=" + type);
+			return _client.Execute(request, Method.GET).Content;
 		}
 
-		public void GetMovieByID(string iMDbID)
+		public string GetMovieByID(string iMDbID)
 		{
 			var request = new RestRequest(OMDbConfigReader.ApiUrlMod + OMDbConfigReader.ApiKey + "&i=" + iMDbID);
-			StringContent = _client.Execute(request, Method.GET).Content;
+			return _client.Execute(request, Method.GET).Content;
 		}
+
+
+
 	}
 }
--- OMDb/OMDbService/OMDbService.cs
+++ OMDb/OMDbService/OMDbService.cs
@@ -5,21 +5,34 @@ namespace OMDb
 {
 	public class OMDbService
 	{
+		public OMDbCallManager OMDbCallManager { get; set; } = new OMDbCallManager();
+
 		public MovieDTO MovieDTO { get; set; } = new MovieDTO();
 
 		public string Movie { get; set; }
 
 		public JObject Json_Movie { get; set; }
 
-		public OMDbService() {}
-
-		public OMDbService(string StringContent)
+		public void Deserialize()
 		{
-			Movie = StringContent;
-
 			Json_Movie = JsonConvert.DeserializeObject<JObject>(Movie);
 
 			MovieDTO.DeserializeRates(Movie);
+		}
+
+		public OMDbService() {}
+
+		public OMDbService(string iMDbID)
+		{
+			Movie = OMDbCallManager.GetMovieByID(iMDbID);
+
+			Deserialize();
+		}
+		public OMDbService(string movieTitle, string year, string season, string episode, string type)
+		{
+			Movie = OMDbCallManager.GetMovieByTitle(movieTitle, year, season, episode, type);
+
+			Deserialize();
 		}
 	}
 }
