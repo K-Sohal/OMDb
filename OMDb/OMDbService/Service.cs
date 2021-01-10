a86eac197c92ccb3cbc9fcd44bba6fde50715e37
KSohal <kavannn@live.com>
2021-01-10 21:06:01 +0000

    updated proj

---
+++ OMDb/OMDbService/Service.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace OMDb
+{
+	public class Service
+	{
+		public OMDbCallManager OMDbCallManager { get; set; } = new OMDbCallManager();
+
+		public DTO DTO { get; set; } = new DTO();
+
+		public string Movie { get; set; }
+
+		public JObject Json_Movie { get; set; }
+
+		public void Deserialize()
+		{
+			Json_Movie = JsonConvert.DeserializeObject<JObject>(Movie);
+
+			DTO.DeserializeRates(Movie);
+		}
+
+		public Service(string iMDbID)
+		{
+			Movie = OMDbCallManager.GetMovieByID(iMDbID);
+
+			Deserialize();
+		}
+		public Service(string movieTitle, string year, string season, string episode, string type)
+		{
+			Movie = OMDbCallManager.GetMovieByTitle(movieTitle, year, season, episode, type);
+
+			Deserialize();
+		}
+	}
+}
